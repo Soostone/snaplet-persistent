6158859f24bd43b86c5869a380472747af711d4c
Doug Beardsley <doug.beardsley@soostone.com>
2013-01-06 13:51:45 -0500

    Added more convenience functions

--- src/Snap/Snaplet/Persistent.hs
+++ src/Snap/Snaplet/Persistent.hs
@@ -3,12 +3,19 @@
 module Snap.Snaplet.Persistent where
 
 -------------------------------------------------------------------------------
-import Control.Monad.State
-import Control.Monad.Trans.Resource
-import Data.Configurator
-import Database.Persist.Postgresql hiding (get)
-import Snap.Snaplet
-import Paths_snaplet_persistent
+import           Control.Monad.State
+import           Control.Monad.Trans.Resource
+import           Data.ByteString (ByteString)
+import           Data.Configurator
+import           Data.Maybe
+import           Data.Readable
+import           Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import           Database.Persist.Postgresql hiding (get)
+import           Database.Persist.Store
+import           Snap.Snaplet
+import           Paths_snaplet_persistent
 -------------------------------------------------------------------------------
 
 
@@ -41,4 +48,40 @@ runPersist :: SqlPersist (ResourceT IO) a -> Handler b (PersistState) a
 runPersist action = do
   pool <- gets persistPool
   liftIO . runResourceT $ runSqlPool action pool
+
+
+-------------------------------------------------------------------------------
+mkKey :: Int -> Key entity
+mkKey = Key . toPersistValue
+
+
+-------------------------------------------------------------------------------
+mkKeyBS :: ByteString -> Key entity
+mkKeyBS = mkKey . fromMaybe (error "Can't ByteString value") . fromBS
+
+
+-------------------------------------------------------------------------------
+mkKeyT :: Text -> Key entity
+mkKeyT = mkKey . fromMaybe (error "Can't Text value") . fromText
+
+
+-------------------------------------------------------------------------------
+showKey :: Key e -> Text
+showKey = T.pack . show . mkInt
+
+
+-------------------------------------------------------------------------------
+showKeyBS :: Key e -> ByteString
+showKeyBS = T.encodeUtf8 . showKey
+
+
+-------------------------------------------------------------------------------
+mkInt :: Key a -> Int
+mkInt = fromPersistValue' . unKey
+
+
+-------------------------------------------------------------------------------
+fromPersistValue' :: PersistField c => PersistValue -> c
+fromPersistValue' = either (const $ error "Persist conversion failed") id
+                    . fromPersistValue
 
