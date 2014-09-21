445dcb8209433255f34ab6589c84402b24e166aa
Carsten König <carsten@gettingsharper.de>
2014-09-21 11:16:47 +0200

    added sqlite backend

---
+++ src/Snap/Snaplet/Persistent/Sqlite.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies      #-}
+
+module Snap.Snaplet.Persistent.Sqlite
+  ( initPersistSqlite
+  , PersistState(..)
+  , HasPersistPool(..)
+  , mkSqlitePool
+  , mkSnapletSqlitePool
+  , runPersist
+  , withPool
+  -- * Utility Functions
+  , mkKey
+  , mkKeyBS
+  , mkKeyT
+  , showKey
+  , showKeyBS
+  , mkInt
+  , mkWord64
+  , followForeignKey
+  , fromPersistValue'
+  ) where
+
+-------------------------------------------------------------------------------
+import           Control.Monad.Logger
+import           Control.Monad.State
+import           Data.Configurator
+import           Data.Configurator.Types
+import           Database.Persist
+import           Database.Persist.Sqlite (ConnectionPool, SqlBackend, SqlPersistT)
+import qualified Database.Persist.Sqlite  as DB
+import           Snap.Snaplet
+import           Snap.Snaplet.Persistent
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- | Initialize Sqlite-Persistent with an initial SQL function called right
+-- after the connection pool has been created. This is most useful for
+-- calling migrations upfront right after initialization.
+--
+-- Example:
+--
+-- > initPersist (runMigrationUnsafe migrateAll)
+--
+-- where migrateAll is the migration function that was auto-generated
+-- by the QQ statement in your persistent schema definition in the
+-- call to 'mkMigrate'.
+initPersistSqlite :: SqlPersistT (NoLoggingT IO) a -> SnapletInit b PersistState
+initPersistSqlite migration = initPersist mkSqlitePool migration
+
+-------------------------------------------------------------------------------
+-- | Constructs a connection pool from Config.
+mkSqlitePool :: MonadIO m => Config -> m ConnectionPool
+mkSqlitePool conf = do
+  sqlConStr <- liftIO $ require conf "sqlite-con-str"
+  cons <- liftIO $ require conf "sqlite-pool-size"
+  DB.createSqlitePool sqlConStr cons
+
+
+-------------------------------------------------------------------------------
+-- | Conscruts a Postgres connection pool in a snaplet context.
+mkSnapletSqlitePool :: (MonadIO (m b v), MonadSnaplet m) => m b v ConnectionPool
+mkSnapletSqlitePool = do
+  conf <- getSnapletUserConfig
+  mkSqlitePool conf
+
+------------------------------------------------------------------------------
+-- | Follows a foreign key field in one entity and retrieves the corresponding
+-- entity from the database.
+followForeignKey :: (PersistEntity a, HasPersistPool m,
+                     PersistEntityBackend a ~ SqlBackend)
+                 => (t -> Key a) -> Entity t -> m (Maybe (Entity a))
+followForeignKey toKey (Entity _ val) = do
+    let key' = toKey val
+    mval <- runPersist $ DB.get key'
+    return $ fmap (Entity key') mval
